85f3db4e4178ad322ee2ef064daf0261a3087606
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-06-28 19:16:52 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+using namespace std;
+int main() {
+
+	ifstream dosya("ornek.txt");//dosya açıldı
+		if (!dosya) {
+			cout << "Dosya açılamadı" << endl;//açılamam durumunda hata verildi
+				return 1;
+		}
+	string satir;//satır nesnesi olusturuldu
+		vector<string> satirlar;//satirlar adında string turunde vector olusturuldu
+	while (getline(dosya, satir)) {//satır sonuna gelene kadar dosyadakiler satır nesnesine aktarıldı
+		satirlar.push_back(satir);//satir nesensinin hepsi satırlar vectorune aktarıldı
+
+	}
+	dosya.close();//dosya kapatıldı
+	for (const string& s : satirlar) {
+		cout << s << endl;
+	}
+}
