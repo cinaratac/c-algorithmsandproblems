825683541d4d6205a1d0bf90dce0c1178fff6430
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-07-03 18:17:50 +0300

    Add files via upload

---
+++ toplanmanptedefetr.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int toplam = 0;//toplam 0 ile başlar
+    ifstream dosya("ornek.txt");//dosyayı aç
+    string satir;//satir olustur
+    int sayi;
+    while (getline(dosya, satir)) {//dosyaları satıra aktar
+        stringstream ss(satir);//satırları böl ve buna ss de
+        string parca;
+        while (getline(ss, parca, ',')) { // ss leri int e çevirmek için parçalara böl
+
+            stringstream parca_ss(parca);//parcaları inte cevirme için yeniden parcala
+            while (parca_ss >> sayi) {//parcaları sayılara aktar
+                toplam += sayi;//sayıları topla
+            }
+        }
+    }
+    cout << "Toplam:" << toplam << endl;//toplam yaz
+}
