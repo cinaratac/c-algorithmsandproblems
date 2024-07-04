0139d98dc0900fd859b1396e240bd02a10c7a153
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-07-04 15:22:11 +0300

    Add files via upload

---
+++ dizi ortalama.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<vector>
+#include<sstream>
+
+using namespace std;
+
+void ortalamahesapla() {
+	vector<double> dizi;
+	double girdi;
+	double toplam = 0.0;
+
+	cout << "Dizi elemanlarını girin (Girişi sonlandırmak için 0 girin):" << endl;
+
+	while (true) {
+		cout << "Eleman " << dizi.size() + 1 << ":" << endl;
+		cin >> girdi;
+		if (girdi == 0) break;
+		dizi.push_back(girdi);
+		toplam += girdi;
+	}
+	double ortalama = toplam / dizi.size();
+	cout << "ORTLAMANIZ:" << ortalama << endl;
+
+}
+
+int main() {
+	cout << "Ortalama hesaplama mainesine hosgeldiniz" << endl;
+	char c;
+	do {
+		ortalamahesapla();
+		cout << "Yeni bir dizi baslatmak icin" << " e' " << "ya basın" << endl;
+			cin >> c;
+
+	}
+			while (c == 'e');
+	
+	cout << "İyi günler" << endl;
+
+
+}
