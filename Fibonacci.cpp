e543dcc29a96a4cc174bc4ab35c23871627e0481
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-07-07 06:08:03 +0300

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int girdi;
+    cout << "dizinizin kaç boyutlu olmasını istiyorsunuz" << endl;
+    cin >> girdi;
+    if (girdi <= 0) {
+        cout << "Ltfen pozitif bir sayı giriniz" << endl;
+        return 1;
+    }
+    vector<int> dizi(girdi);
+    dizi[0] = 1;
+    if(girdi>1){
+        dizi[1]=1;
+         }
+    for (int i = 2; i < girdi; i++) {
+        dizi[i] = dizi[i - 1] + dizi[i - 2];
+        }
+    // Fibonacci serisini yazdırma
+    cout << "Fibonacci serisi: ";
+    for (int i = 0; i < girdi; i++) {
+        cout << dizi[i];
+        if (i < girdi - 1) {
+            cout << ",";
+        }
+    }
+    cout << endl;
+
+
+
+}
