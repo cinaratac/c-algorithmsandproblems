c8352fbb1b7ea55be74409aa75ad8c4504cc8179
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-07-09 04:34:36 +0300

    Add files via upload

---
+++ asalsayıbulinatör.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+    bool isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for(int i=2;i<num;i++){
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    int main(){
+        cout << "Başlangıc değeri" << endl;
+            int low;
+            int max;
+            cin >> low;
+            cout << "Bıtıs degeri:" << endl;
+            cin >> max;
+            for (int i = low; i <= max; i++) {
+                if (isPrime(i)) {
+                    cout << i << ",";
+                }
+            }
+
+
+}
