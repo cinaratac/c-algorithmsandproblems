5e18a3fb0b5885e72327fd769a77d5604ac29897
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-07-10 02:42:25 +0300

    Add files via upload

---
+++ ortalamabulinatör.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+char Harfbulma(double avarage){
+	if (avarage >= 90) return 'A';
+	else if (avarage >= 80) return'B';
+	else if (avarage >= 70) return 'C';
+	else if (avarage >= 60) return 'D';
+	else return 'F';
+}
+int main() {
+	int derssayi;
+	cout << "Ders sayınızı giriniz" << endl;
+	cin >> derssayi;
+	int toplam=0;
+	vector<int> dersler(derssayi);
+	for (int i = 0; i < derssayi; i++) {
+		cout << i+1 << ". dersin notunu giriniz" << endl;
+		cin >> dersler[i];
+		toplam += dersler[i];
+	}
+	double avarage = toplam / derssayi;
+	char harf = Harfbulma(avarage);
+	cout << "ORTALAMA:" << avarage << endl;
+	cout<< "HARF:"<<harf<<endl;
+
+
+
+
+}
+
+
