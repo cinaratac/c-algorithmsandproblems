19c8f5f506cd85017fc5184c0c048ef6e7259a26
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-09-14 00:10:59 +0300

    Update setveget fonksiyon.cpp

--- setveget fonksiyon.cpp
+++ setveget fonksiyon.cpp
@@ -1,36 +1,39 @@
-﻿#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-class Öğrenci {
+
+class Ogrenci {
 private:
 	string isim;
 	int yas;
 public:
 	//yapıcı
-	Öğrenci(const string& i, int y) : isim(i) , yas(y){}
-
+	Ogrenci(const string& i, int y) : isim(i), yas(y) {}
+	void setİsim(const string& i) {
+		isim = i;
+	}
+	void setYas(int y) {
+		yas = y;
+	}
 	string getİsim() const {
 		return isim;
 	}
 	int getYas() const {
 		return yas;
 	}
-	void setİsim(const string& i) {
-		isim = i;
-	}
-	void setYas(int y) {
-		if (yas >= 0) {
-			yas = y;
-		}
-	}
+
 };
 int main() {
-	Öğrenci ö1("Mahmut", 25);
-	cout << ö1.getİsim() << " " << ö1.getYas() << endl;
-
-	ö1.setİsim("Mwhmet");
-	ö1.setYas(27);
-
-	cout << ö1.getİsim() << " " << ö1.getYas() << endl;
-
+	Ogrenci o1("ANAN", 54);
+	string isimg;
+	int yasg;
+	cout << o1.getİsim() << " " << o1.getYas() << endl;
+	cout << "Yeni ismi yazınız" << endl;
+	cin >> isimg;
+	cout << "Yeni yası yazınız" << endl;
+	cin >> yasg;
+	o1.setİsim(isimg);
+	o1.setYas(yasg);
+	cout << o1.getİsim() << " " << o1.getYas() << endl;
+ 
 }
