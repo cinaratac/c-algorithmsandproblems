4f7cb9012490e8a824bb649db68366cc7605f2a0
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-09-15 00:11:09 +0300

    Update ortalamabulinatör.cpp

--- ortalamabulinatör.cpp
+++ ortalamabulinatör.cpp
@@ -1,34 +1,37 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-char Harfbulma(double avarage){
+char Harfbulma(double avarage) {
 	if (avarage >= 90) return 'A';
-	else if (avarage >= 80) return'B';
+	else if (avarage >= 80) return 'B';
 	else if (avarage >= 70) return 'C';
 	else if (avarage >= 60) return 'D';
 	else return 'F';
 }
+
 int main() {
-	int derssayi;
-	cout << "Ders sayınızı giriniz" << endl;
+	char harf;
+	double derssayi;
+	int toplam = 0;
+	cout << "Ders sayisini giriniz" << endl;
 	cin >> derssayi;
-	int toplam=0;
 	vector<int> dersler(derssayi);
 	for (int i = 0; i < derssayi; i++) {
-		cout << i+1 << ". dersin notunu giriniz" << endl;
+		cout << i + 1 << ". dersi giriniz" << endl;
 		cin >> dersler[i];
-		toplam += dersler[i];
+		if (dersler[i] >= 0 && dersler[i] <= 100) {
+			toplam += dersler[i];
+		}
+		else {
+			cout << "Lütfen geçerli bir sayı giriniz" << endl;
+			i--;
+
+		}
 	}
 	double avarage = toplam / derssayi;
-	char harf = Harfbulma(avarage);
-	cout << "ORTALAMA:" << avarage << endl;
-	cout<< "HARF:"<<harf<<endl;
-
-
-
-
+	harf = Harfbulma(avarage);
+	cout << "Notunuz:" << harf << endl;
+	cout << "Ortalamanız" << avarage << endl;
 }
-
-
